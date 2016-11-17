014a882ddb07a999adea765730e9a8eabf2872f3
Vladislav Dugnist <vdugnist@gmail.com>
2016-11-17 22:35:24 +0300

    add documentation for ABManager

--- AsyncBlur/AsyncBlur/ABManager.h
+++ AsyncBlur/AsyncBlur/ABManager.h
@@ -10,15 +10,75 @@
 
 @class UIImage, UIImageView;
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface ABManager : NSObject
 
+
+/**
+ Blurring on simulator is disabled by default for performance reasons.
+ @param shouldBlurOnSimulator pass here YES to enable blur on simulator. NO by default.
+ */
 + (void)setShouldBlurOnSimulator:(BOOL)shouldBlurOnSimulator;
 
-+ (void)renderBlurForImage:(UIImage *)image andSetInImageView:(UIImageView *)imageView;
-+ (void)renderBlurForImage:(UIImage *)image radius:(CGFloat)radius andSetInImageView:(UIImageView *)imageView;
 
-+ (void)renderBlurForImage:(UIImage *)image forImageView:(UIImageView *)imageView withCallback:(void (^)(UIImage *blurredImage))callback;
-+ (void)renderBlurForImage:(UIImage *)image forImageView:(UIImageView *)imageView radius:(CGFloat)radius withCallback:(void (^)(UIImage *blurredImage))callback;
+/**
+ Blur image with default radius and set in image view. If image view is nil, nothing will happen.
+ Image view is also used for optimization: if you call this method with one image view and different images, 
+ only last operation in queue will be executed. It's useful for blurring background in table view cells or other
+ collections. 
+ 
+ @param image image to blur
+ @param imageView imageView to set result in
+ */
++ (void)renderBlurForImage:(UIImage *)image andSetInImageView:(nullable UIImageView *)imageView;
 
+
+/**
+ Blur image with given radius and set in image view. If image view is nil, nothing will happen.
+ With zero blur radius you'll get original image.
+ Image view is also used for optimization: if you call this method with one image view and different images
+ or with different blur radius, only last operation in queue will be executed.
+
+ @param image image to blur
+ @param radius blur radius
+ @param imageView image view to set result in
+ */
++ (void)renderBlurForImage:(UIImage *)image radius:(CGFloat)radius andSetInImageView:(nullable UIImageView *)imageView;
+
+
+
+/**
+ Blur image with default radius. If callback is passed, will not set result in image view.
+ If callback is not passed, will set result in image view.
+ Image view is also used for optimization: if you call this method with one image view and different images,
+ only last operation in queue will be executed. It's useful for blurring background in table view cells or other
+ collections.
+ 
+ @param image image to blur
+ @param imageView image view to set result in
+ @param callback callback with result
+ */
++ (void)renderBlurForImage:(UIImage *)image
+              forImageView:(nullable UIImageView *)imageView
+              withCallback:(void (^ _Nullable)(UIImage *blurredImage))callback;
+
+/**
+ Blur image with given radius. If callback passed, will not set result in image view.
+ If callback is not passed, will set result in image view.
+ Image view is also used for optimization: if you call this method with one image view and different images
+ or with different blur radius, only last operation in queue will be executed.
+ 
+ @param image image to blur
+ @param imageView image view to set result in
+ @param raius blur radius
+ @param callback callback with result
+ */
++ (void)renderBlurForImage:(UIImage *)image
+              forImageView:(nullable UIImageView *)imageView
+                    radius:(CGFloat)radius
+              withCallback:(void (^ _Nullable)(UIImage *blurredImage))callback;
 
 @end
+
+NS_ASSUME_NONNULL_END
